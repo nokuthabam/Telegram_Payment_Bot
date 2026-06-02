e95dd884a20df31d512a4dcd6dd99ad896aa404c
Nokuthaba <nokuthaba.m@outlook.com>
2026-06-02 21:45:33 +0200

    Update

--- bot.py
+++ bot.py
@@ -23,6 +23,9 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
 db = Database()
